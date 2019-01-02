78c845be0cb549b017bf8fc06dbf3325fd56de21
daniel <developmentleviathan@gmail.com>
2019-01-01 20:49:19 -0500

    p

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://beevrr.herokuapp.com/'),
+    'url' => env('APP_URL', 'beevrr.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
