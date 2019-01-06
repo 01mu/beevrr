fd59437a7fb454c60e190584265d545e904c0f16
daniel <developmentleviathan@gmail.com>
2019-01-06 15:11:49 -0500

    discussion likes

--- config/global.php
+++ config/global.php
@@ -6,4 +6,4 @@
 
 return [    'pagination' => 10,
             'an_hour' => 60 * 60, /* for debugging */
-            'use_https' => 0,];
+            'use_https' => 1,];
--- database/migrations/2019_01_06_200624_likes_disc.php
+++ database/migrations/2019_01_06_200624_likes_disc.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class LikesResp extends Migration
+class LikesDisc extends Migration
 {
     public function up()
     {
